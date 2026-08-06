d4da0b5893b6de8537213b7e9340e2bccc6fb60f
bronya <blobcat@tuta.io>
2026-08-06 18:00:59 +0200

    Add user tags

---
+++ PlumbobForge.Backend/Migrations/20260801125011_AddIsUserTagged.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlumbobForge.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsUserTagged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUserTagged",
+                table: "MetaEntities",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUserTagged",
+                table: "MetaEntities");
+        }
+    }
+}
---
+++ PlumbobForge.Backend/Migrations/20260801130748_AddUserTags.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlumbobForge.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserTags",
+                table: "MetaEntities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserTags",
+                table: "MetaEntities");
+        }
+    }
+}
